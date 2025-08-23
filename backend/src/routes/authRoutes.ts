aa02c9cd84fc2e374fd97b3a7479b5f663b8f8e5
Fernanda Santo <fgesanto@gmail.com>
2025-08-22 19:32:45 -0700

    updated outhRoutes.ts to remove the logout

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,7 +6,6 @@ const router: Router = Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
 
 router.get('/me', requireAuth, authController.getMe);
 
